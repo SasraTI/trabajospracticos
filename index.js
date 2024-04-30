8f6d01b135952bec780c495b527f7615247a9949
Sasra <salazar.sntiago@gmail.com>
2024-04-30 18:55:53 -0300

    mi primer commit

---
+++ index.js
@@ -0,0 +1,78 @@
+import express from 'express';
+import fs from "fs";
+import bodyParser from "body-parser";
+
+const app = express();
+app.use(bodyParser.json());
+
+const readData = () => {
+    try{
+    const data = fs.readFileSync("./db.json");
+    return JSON.parse(data);
+    } catch(error) {
+        console.log(error);
+    }
+};
+
+const writeData = (data) => {
+    try{
+        fs.writeFileSync("./db.json", JSON.stringify(data));
+        } catch(error) {
+            console.log(error);
+        }
+};
+
+app.get("/books", (req,res) => {
+    const data = readData();
+    res.json(data.books);
+});
+
+app.get("/books/:id", (req, res) => {
+    const data = readData();
+    const id = parseInt(req.params.id);
+    const book = data.books.find((book) => book.id === id);
+    res.json(book);
+})
+app.post("/books", (req,res) => {
+    const data = readData();
+    const body = req.body;
+    const newbook = {
+       id: data.books.length + 1,
+       ...body,
+    };
+    data.books.push(newbook);
+    writeData(data);
+    res.json(newbook);
+});
+
+app.put("/books/:id", (req,res) => {
+    const data = readData();
+    const body = req.body;
+    const id = parseInt(req.params.id);
+    const bookIndex = data.books.findIndex((book) => book.id === id);
+    data.books[bookIndex] = {
+        ...data.books[bookIndex],
+        ...body
+    };
+    writeData(data);
+    res.json({message: "El grupo se actualizó correctamente"});
+});
+
+app.delete("/books/:id", (req,res) => {
+    const data = readData();
+    const id = parseInt(req.params.id);
+    const bookIndex = data.books.findIndex((book) => book.id === id);
+    data.books.splice(bookIndex, 1);
+    writeData(data);
+    res.json({message: "El grupo fue borrado correctamente"});
+});
+
+readData();
+
+app.get("/", (req, res) => {
+res.send("Hola, soy bonita mwha")
+});
+
+app.listen(3000, () =>{
+    console.log("El servidor está en el puerto 3000");
+});
